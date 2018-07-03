6752ea6018e4f6218430ad6fd72ee41b4665b9fe
EwanHou <ewan520@hotmail.com>
2018-07-04 00:29:57 +0800

    index cotent

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,36 +1,52 @@
 <template>
 <div class="wrap">
 
-<div class="header">
-{{comicHeader}}
+  <div class="header">
+    {{comicHeader}}
+  </div>
+
+  <div class="content">
+    <div class="item comicPic">
+      <img :src="comicImg">
+    </div>
+    <div class="item comicInfo">
+      <div class="title">
+        MY HEXSCHOOL
+      </div>
+      <div class="info">
+      <div class="detail">
+       <div class="data"><span class="col1">Genres</span><span class="col2">Fusce/vehicula/dolor</span></div>
+       <div class="data"><span class="col1">Author</span><span class="col2">Namae Shiranai</span></div>
+       <div class="data"><span class="col1">Status</span><span class="col2">Ongoing</span></div>
+       <div class="data"><span class="col1">Rate</span><span class="col2">star*4</span></div>
+      </div>
+      <div class="summary">
+        <div class="sumTitle">
+          Summary
+        </div>
+        <p class="sumContent">
+        If your banker breaks, you snap; if your apothecary by mistake sends you poison in your pills, you die.<br/><br/>
+        Therefore, I say, I saw that this situation of mine was the precise situation of every mortal that has this Siamese connexion with a plurality of other mortals.
+        </p>
+      </div>
+      </div>
+    </div>
+  <div class="item chapters">
+    <div class="chTitle">
+      All Chapters
+    </div>
+    <div class="chContent">
+      <div class="chDetail">
+Chapter 1: The F2E Challenge Start!
+      </div>
+      <div class="chDetail">
+Chapter 2: Todo List is Going Crazy! <span class="new">New</span>
+      </div>
+    </div>
+  </div>
+  </div>
 </div>
 
-<div class="content">
-<div class="item comicPic">
-<img :src="comicImg">
-</div>
-<div class="item comicInfo">
-<div class="title">
-MY HEXSCHOOL
-</div>
-<div class="info">
-123
-</div>
-<div class="summary">
-Summary
-</div>
-</div>
-<div class="item chaprers">
-<div class="title">
-All Chapters
-</div>
-<div class="content">
-content
-</div>
-</div>
-</div>
-
-</div>
 </template>
 
 <script>
@@ -48,16 +64,18 @@ export default {
 </script>
 
 <style lang='scss'>
-@import url("https://fonts.googleapis.com/css?family=Roboto:700i");
+@import url("https://fonts.googleapis.com/css?family=Roboto:400,700i");
 $green: #50ff44;
 $black: #000000;
+$white: #ffffff;
 $_font: "Roboto", sans-serif;
 * {
   margin: 0;
   padding: 0;
   list-style: none;
   line-height: 1;
-  outline: 1px solid #ff0000;
+  // outline: 1px solid #ff0000;
+  font-family: $_font;
 }
 
 .wrap {
@@ -73,12 +91,14 @@ $_font: "Roboto", sans-serif;
   .header {
     color: $green;
     font-family: $_font;
+    font-style: italic;
+    font-weight: bold;
     background-color: $black;
     font-size: 42px;
     padding: 12px;
   }
   .content {
-    margin: 3% 10%;
+    padding: 3% 15%;
     .item {
       position: relative;
       float: left;
@@ -87,7 +107,7 @@ $_font: "Roboto", sans-serif;
       width: 50%;
       img {
         border: black solid 5px;
-        width: 98%;
+        width: 99%;
         height: 600px;
         vertical-align: middle;
       }
@@ -95,14 +115,67 @@ $_font: "Roboto", sans-serif;
     .comicInfo {
       width: 50%;
       .title {
-        color: white;
+        color: $white;
         background-color: $black;
         font-family: $_font;
+        font-weight: bold;
         font-size: 50px;
+        padding: 5px;
+        margin-left: 5px;
+      }
+      .info {
+        padding: 30px;
+        text-align: left;
+        font-size: 22px;
+        color: $black;
+        .detail {
+          display: table;
+          margin-bottom: 20%;
+          border-spacing: 13px;
+          .data {
+            display: table-row;
+            .col1 {
+              display: table-cell;
+              font-weight: bold;
+              padding-right: 5%;
+            }
+            .col2 {
+              display: table-cell;
+            }
+          }
+        }
+      }
+      .summary {
+        .sumTitle {
+          font-weight: bold;
+          margin-bottom: 15px;
+        }
       }
     }
-    .chaprers {
+    .chapters {
       width: 100%;
+      text-align: left;
+      margin-top: 5%;
+      .chTitle {
+        background-color: $black;
+        color: $white;
+        font-size: 28px;
+        font-weight: bold;
+        width: 18%;
+        padding: 10px;
+      }
+      .chContent {
+        border: black solid 3px;
+        color: $black;
+        font-size: 18px;
+        font-weight: bold;
+        .chDetail {
+          padding: 12px;
+          .new {
+            background-color: $green;
+          }
+        }
+      }
     }
   }
 }
